7279521aa33e52ecd53e2cc8864911cf438cf6f5
Claudio <claudiocli.cp@gmail.com>
2021-11-28 11:55:49 +0100

    Pass

--- backend/lessons/views.py
+++ backend/lessons/views.py
@@ -1,3 +1,11 @@
+from rest_framework import generics, permissions, viewsets
+
 from django.shortcuts import render
 
 # Create your views here.
+
+class LessonList(generics.ListCreateAPIView):
+	# permission_classes = [IsAuthorOrReadOnly, permissions.IsAuthenticated]
+	# queryset = Post.objects.all()
+	# serializer_class = PostSerializer
+	pass
