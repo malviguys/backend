eaab52f3d906c00a601f96233bb019fb1050e3de
Paola Guarasci <paolaguarasci@gmail.com>
2021-12-03 11:35:51 +0100

    secure admin path

--- myawesomeproject/myawesomeproject/urls.py
+++ myawesomeproject/myawesomeproject/urls.py
@@ -1,14 +1,19 @@
+from dotenv import load_dotenv
+load_dotenv()
 from django.urls import include, path
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.schemas import get_schema_view
 from rest_framework.documentation import include_docs_urls
+import os
+
 
 API_TITLE="Lesson Booking API"
 API_DESCRIPTION="Lessons"
+PATH_ADMIN = os.environ.get("URL_ADMIN")
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path(PATH_ADMIN, admin.site.urls),
     path('docs/', include_docs_urls(title=API_TITLE, description=API_DESCRIPTION)),
     path('schema/', get_schema_view(title=API_TITLE)),
     path('api/v1/lessons', include('restapi.urls')),
